4d4ae921328460e4dcd5515d5235ba2de3b7ec0f
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2026-05-28 21:33:02 -0600

    asd

--- WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fda493b1cbd423b25f057bd1e423057a61e5cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf1048f6b704d84369c091c41abccb2cc21e9a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
