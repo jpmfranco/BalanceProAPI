db8b44faf3e524438c1b32371c5a38cccec2681e
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2026-05-29 18:57:31 -0600

    push

--- WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d4ae921328460e4dcd5515d5235ba2de3b7ec0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea7939888cb18862fc83b898d147603c2ff344e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
